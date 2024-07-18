5679f10f1c0cb9730ac3f7d997d28baeb91199d4
TheClaireLiu <cliu202211@gmail.com>
2024-07-17 23:30:14 -0400

    [ADD] send post like Facebook

---
+++ react-crash-course/src/App.jsx
@@ -0,0 +1,15 @@
+import Post from "./components/Post.jsx";
+
+function App(){
+
+  return (
+    <main>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
+}
+
+export default App;
