964e44ee80f25fce2184bdb07f36d369111b5cec
TheClaireLiu <cliu202211@gmail.com>
2025-06-25 11:47:06 -0400

    [use useFormState() instead of useActionState()  in React18.]

--- 05-onwards-foodies-starting-project/components/meals/meals-form-submit.js
+++ 05-onwards-foodies-starting-project/components/meals/meals-form-submit.js
@@ -1,13 +1,9 @@
 'use client'
-import {useActionState} from "react";
+import {useFormStatus} from "react-dom";
 
-// 一个空的处理函数（暂时占位）
-async function dummyAction(_, __) {
-  return {};
-}
 export default function MealsFormSubmit(){
   //use object destructuring to pull out a property from
-  const [_, __, pending] = useActionState(dummyAction, {});
+  const { pending } = useFormStatus();
 
   return <button disabled={pending}>
     {pending ? "Submitting..." : "Share Meal"}
