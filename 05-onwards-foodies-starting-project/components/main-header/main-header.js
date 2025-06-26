0b95e2ddbf1786eaebc8ef35829f8879992781c0
TheClaireLiu <cliu202211@gmail.com>
2025-06-25 22:16:38 -0400

    [Store Uploaded Images in the Cloud(AWS S3) and add dynamic Metadata.]

--- 05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -13,7 +13,7 @@ export default function MainHeader(){
     <>
       <MainHeaderBackground />
       <header className={classes.header}>
-        <Link className={classes.logo} href="/05-onwards-foodies-starting-project/public">
+        <Link className={classes.logo} href="/">
           <Image src={logoImg} alt="A plate with food on it" priority/>
           NextLevel Food
         </Link>
